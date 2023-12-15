9bdb8ab29c051e2c2da253821382103b574d4422
andrey <mist3s@github.com>
2023-12-15 17:58:36 +0200

    Добавить вьюсет для модели Таг.

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,5 +4,15 @@ from rest_framework.generics import get_object_or_404
 from djoser.views import UserViewSet
 
 from users.models import Follow, User
+from recipes.models import Tag, Ingredient, Recip
+from .serializers import TagSerializer
 
 
+class TagViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
+    """Вьюсет для модели Таг."""
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
