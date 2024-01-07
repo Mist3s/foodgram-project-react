52d7a97be7d7c119257866f935e39fd6f17e5284
andrey <mist3s@github.com>
2024-01-07 18:05:09 +0200

    flake8

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -5,6 +5,6 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
     """Разрешение: чтение - все, редактирование - автор."""
     def has_object_permission(self, request, view, obj):
         return (
-                request.method in permissions.SAFE_METHODS
-                or obj.author == request.user
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
         )
